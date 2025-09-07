db836cb18b65d2876deee43cc3abd646c81544bb
unknown <malailiyati107@gmail.com>
2025-09-08 06:41:24 +0700

    feat: setup main.go with DB connection, router, and swagger docs

---
+++ cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+
+	"github.com/malailiyati/backend/internal/configs"
+	"github.com/malailiyati/backend/internal/routers"
+)
+
+// @title 			tickitz
+// @version 		1.0
+// @description 	ticketing
+// @host			localhost:8080
+// @basePath		/
+
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+func main() {
+	log.Println(os.Getenv("DBUSER")) // ini langsung kebaca dari .env
+
+	db, err := configs.InitDB()
+	if err != nil {
+		log.Println("Failed to connect DB:", err)
+		return
+	}
+	defer db.Close()
+
+	if err := configs.TestDB(db); err != nil {
+		log.Println("Ping DB failed:", err)
+		return
+	}
+	log.Println("DB Connected")
+
+	r := routers.InitRouter(db)
+
+	r.Run(":8080")
+}
