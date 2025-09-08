87ea94b0f60667313941e4496d64858069f0e630
unknown <malailiyati107@gmail.com>
2025-09-08 07:17:53 +0700

    feat(routers): add admin movie routes with JWT middleware

---
+++ internal/routers/admin.router.go
@@ -0,0 +1,25 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/malailiyati/backend/internal/handlers"
+	"github.com/malailiyati/backend/internal/middlewares"
+	"github.com/malailiyati/backend/internal/repositories"
+)
+
+func InitMovieAdminRouter(router *gin.Engine, db *pgxpool.Pool) {
+	movieAdminRepo := repositories.NewMovieAdminRepository(db)
+	movieAdminHandler := handlers.NewMovieAdminHandler(movieAdminRepo)
+
+	// Admin routes
+	admin := router.Group("/admin")
+	admin.Use(middlewares.VerifyToken)
+	{
+		admin.GET("/movies", movieAdminHandler.GetAllMovies)
+		admin.DELETE("/movies/:id", movieAdminHandler.DeleteMovie)
+		admin.PATCH("/movies/:id", movieAdminHandler.UpdateMovie)
+	}
+
+}
