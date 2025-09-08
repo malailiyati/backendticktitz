a2716ef811b7c52216e5baac4d675d5c3ae78b82
unknown <malailiyati107@gmail.com>
2025-09-08 07:05:41 +0700

    feat(models): add User, UserBody, and UserAuth models for user management and auth

---
+++ internal/models/user.model.go
@@ -0,0 +1,25 @@
+package models
+
+import "time"
+
+// import "mime/multipart"
+
+type User struct {
+	Id        int       `db:"id" json:"id"`
+	Email     string    `db:"email" json:"email"`
+	Role      string    `db:"role" json:"role,omitempty"`
+	Password  string    `db:"password" json:"password,omitempty"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+	// Images   string `db:"images" json:"image"`
+}
+
+type UserBody struct {
+	User
+	// Images *multipart.FileHeader `form:"image"`
+}
+
+type UserAuth struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required,min=8"`
+}
