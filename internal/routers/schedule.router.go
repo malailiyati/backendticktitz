a4f47a1ff62892cea4f02b6ff74b3cdc1ea7ad5e
unknown <malailiyati107@gmail.com>
2025-09-08 07:20:55 +0700

    feat(routers): add schedule route to fetch movie schedules

---
+++ internal/routers/schedule.router.go
@@ -0,0 +1,16 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/malailiyati/backend/internal/handlers"
+	"github.com/malailiyati/backend/internal/repositories"
+)
+
+func InitScheduleRouter(router *gin.Engine, db *pgxpool.Pool) {
+	scheduleRepo := repositories.NewScheduleRepository(db)
+	scheduleHandler := handlers.NewScheduleHandler(scheduleRepo)
+
+	router.GET("/schedule", scheduleHandler.GetSchedulesByMovie)
+}
