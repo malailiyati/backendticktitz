d84d79e03c8e95af2c48ae2f86c247c1cd725df7
unknown <malailiyati107@gmail.com>
2025-09-15 13:58:14 +0700

    feat: add valide for upload file

---
+++ internal/utils/upload.validate.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"fmt"
+	"mime/multipart"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SaveFile(c *gin.Context, file *multipart.FileHeader, folder, prefix string, id int) (string, string, error) {
+	ext := strings.ToLower(filepath.Ext(file.Filename))
+	allowed := map[string]bool{".jpg": true, ".jpeg": true, ".png": true, ".jfif": true}
+	if !allowed[ext] {
+		return "", "", fmt.Errorf("invalid file type")
+	}
+	if file.Size > 5<<20 {
+		return "", "", fmt.Errorf("file too large")
+	}
+
+	os.MkdirAll("public/"+folder, os.ModePerm)
+	newName := fmt.Sprintf("%s_%d_%d%s", prefix, id, time.Now().UnixNano(), ext)
+	savePath := filepath.Join("public", folder, newName)
+
+	if err := c.SaveUploadedFile(file, savePath); err != nil {
+		return "", "", err
+	}
+
+	// return: relative path (buat DB), full path (buat rollback)
+	return "/" + folder + "/" + newName, savePath, nil
+}
